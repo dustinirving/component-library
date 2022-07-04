54a0e78a74742e053c6e47cbdbd203c7aa48eeb5
Dustin Irving <dustin.irving@gmail.com>
2022-07-03 23:39:41 -0400

    add support for color modes

--- src/components/GlobalStyles/Colors.tsx
+++ src/components/GlobalStyles/Colors.tsx
@@ -1,16 +1,37 @@
 import { createGlobalStyle } from 'styled-components';
 
-const Colors = createGlobalStyle`
-  :root {
-    --color-active: #3A90C5;
-    --color-text: #808080;
-    --color-bg-primary: #fff;
+const lightModeColors = `
+  --color-active: #3A90C5;
+  --color-text: #808080;
+  --color-bg-primary: #fff;
+`;
 
-    @media (prefers-color-scheme: dark) {
-      --color-active: #3AC5B4;
-      --color-text: #ffffff;
-      --color-bg-primary: #2d2d2d;
-    }
+const darkModeColors = `
+  --color-active: #3AC5B4;
+  --color-text: #ffffff;
+  --color-bg-primary: #2d2d2d;
+`;
+
+const systemMode = `
+${lightModeColors}
+
+@media (prefers-color-scheme: dark) {
+  ${darkModeColors}
+}`;
+
+const Colors = createGlobalStyle<{ mode?: 'system' | 'dark' | 'light' }>`
+  :root {
+    ${({ mode }) => {
+      switch (mode) {
+        case 'light':
+          return lightModeColors;
+        case 'dark':
+          return darkModeColors;
+        default:
+          return systemMode;
+      }
+    }}
+    
   }
 `;
 
