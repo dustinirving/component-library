78be79e55354ec2e606ff7f68503946477d5d977
Dustin Irving <dustin.irving@gmail.com>
2022-07-10 14:25:52 -0400

    chore: bump version of storybook dependencies

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,12 +2,7 @@ const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
 
 module.exports = {
   stories: ['../src/**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: [
-    '@storybook/addon-links',
-    '@storybook/addon-essentials',
-    '@storybook/addon-controls',
-    'storybook-addon-designs',
-  ],
+  addons: ['@storybook/addon-essentials'],
   webpackFinal: async (config) => {
     config.resolve.plugins = [
       ...(config.resolve.plugins || []),
